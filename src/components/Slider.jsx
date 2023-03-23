50e9f3ff0d8d0db50324a798a62252e5f03363ac
Kim-EunSu <kes73640@gmail.com>
2023-03-23 10:03:09 +0900

    Slider기능 추가

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import {
   KeyboardArrowLeftOutlined,
   KeyboardArrowRightOutlined,
 } from "@mui/icons-material";
-import left from "../assets/images/left.jpg";
+import { sliderItems } from "../data";
 import styled from "styled-components";
 
 const Conatiner = styled.div`
@@ -10,6 +11,7 @@ const Conatiner = styled.div`
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -28,34 +30,38 @@ const Arrow = styled.div`
   right: ${(props) => props.direction === "right" && "10px"};
   cursor: pointer;
   opacity: 0.5;
+  z-index: 2;
 `;
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
-  /* width: 100vw; */
+  width: 100vw;
   height: 100vh;
   display: flex;
   align-items: center;
+  background-color: ${(props) => props.bg};
 `;
 
 const ImgContainer = styled.div`
+  display: flex;
+  justify-content: center;
   height: 100%;
   flex: 1;
-  border: 3px solid red;
 `;
 
 const Image = styled.img`
-  width: 100%;
   height: 100%;
   object-fit: contain;
 `;
 
 const InfoContainer = styled.div`
   flex: 1;
-  border: 3px solid blue;
   padding: 50px;
 `;
 
@@ -77,27 +83,36 @@ const Button = styled.button`
 `;
 
 function Slider() {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
   return (
     <Conatiner>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <KeyboardArrowLeftOutlined></KeyboardArrowLeftOutlined>
       </Arrow>
-      <Wrapper>
+      <Wrapper slideIndex={slideIndex}>
         {/* 슬라이드 작성 */}
-        <Slide>
-          <ImgContainer>
-            <Image src={left} />
-          </ImgContainer>
-          <InfoContainer>
-            <Title>Summer Sale</Title>
-            <Desc>
-              Don't Compromise on Style! Get Fla 30% off for New Arrivals
-            </Desc>
-            <Button>Shop Now</Button>
-          </InfoContainer>
-        </Slide>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg} key={item.id}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc} </Desc>
+              <Button>Shop Now</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
       </Wrapper>
-      <Arrow direction="right">
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <KeyboardArrowRightOutlined></KeyboardArrowRightOutlined>
       </Arrow>
     </Conatiner>
