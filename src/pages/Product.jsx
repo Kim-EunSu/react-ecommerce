f0e03e6dfa5285472f1e7a8c2026abdf8c6dc08b
Kim-EunSu <kes73640@gmail.com>
2023-03-28 19:02:28 +0900

    Product 수정

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -11,103 +11,134 @@ const Contaier = styled.div``;
 const Wrapper = styled.div`
   padding: 50px;
   display: flex;
+  flex-wrap: wrap;
+  border: 3px solid red;
   ${mobile({ flexDirection: "column" })}
 `;
 
-const ImgContainer = styled.div`
-  flex: 1;
+const Goods = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 20px;
 `;
-const Image = styled.img`
-  width: 100%;
-  height: 90vh;
-  object-fit: cover;
-  ${mobile({ height: "40vh" })}
+
+const Wrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+  background-color: red;
 `;
-const InfoContainer = styled.div`
-  flex: 1;
-  padding: 0px 50px;
-  background-color: #fefefe;
-  ${mobile({ padding: "10px 10px" })}
-`;
+
 const Title = styled.h1`
   font-weight: 200;
-`;
-const Desc = styled.p`
-  margin: 20px 0;
-`;
-const Price = styled.span`
-  font-weight: 100;
-  font-size: 40px;
-  ${mobile({ fontSize: "25px" })}
-`;
-
-const FilterContainer = styled.div`
   width: 50%;
-  margin: 30px 0;
-  display: flex;
-  justify-content: space-between;
-  ${mobile({ width: "100%" })}
-`;
-const Filter = styled.div`
-  display: flex;
-  align-items: center;
-`;
-const FilterTitle = styled.span`
   font-size: 20px;
-  font-weight: 200;
-`;
-const FilterColor = styled.div`
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  margin: 0 5px;
-  cursor: pointer;
-  background-color: ${(props) => props.color};
-`;
-const FilterSize = styled.select`
-  margin-left: 10px;
-  padding: 5px;
 `;
 
-const FilterSizeOption = styled.option``;
-
-const AddContainer = styled.div`
-  width: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  ${mobile({ flexDirection: "column" })}
-`;
-const AmountContainer = styled.div`
-  display: flex;
-  align-items: center;
-  font-weight: 700;
+const Image = styled.img`
+  width: 70%;
+  height: 30vh;
+  object-fit: contain;
+  ${mobile({ height: "40vh" })}
 `;
 
-const Amount = styled.span`
-  width: 30px;
-  height: 30px;
-  border-radius: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 0px 5px;
-  border: 1px solid rgb(132, 165, 157);
-  ${mobile({ marginBottom: "20px" })}
-`;
+// const ImgContainer = styled.div`
+//   flex: 1;
+// `;
+// const Image = styled.img`
+//   width: 100%;
+//   height: 90vh;
+//   object-fit: cover;
+//   ${mobile({ height: "40vh" })}
+// `;
+// const InfoContainer = styled.div`
+//   flex: 1;
+//   padding: 0px 50px;
+//   background-color: #fefefe;
+//   ${mobile({ padding: "10px 10px" })}
+// `;
+// const Title = styled.h1`
+//   font-weight: 200;
+// `;
+// const Desc = styled.p`
+//   margin: 20px 0;
+// `;
+// const Price = styled.span`
+//   font-weight: 100;
+//   font-size: 40px;
+//   ${mobile({ fontSize: "25px" })}
+// `;
 
-const Button = styled.button`
-  padding: 15px;
-  border-radius: 5px;
-  border: 2px solid rgb(132, 165, 157);
-  background-color: white;
-  cursor: pointer;
-  font-weight: 500;
+// const FilterContainer = styled.div`
+//   width: 50%;
+//   margin: 30px 0;
+//   display: flex;
+//   justify-content: space-between;
+//   ${mobile({ width: "100%" })}
+// `;
+// const Filter = styled.div`
+//   display: flex;
+//   align-items: center;
+// `;
+// const FilterTitle = styled.span`
+//   font-size: 20px;
+//   font-weight: 200;
+// `;
+// const FilterColor = styled.div`
+//   width: 20px;
+//   height: 20px;
+//   border-radius: 50%;
+//   margin: 0 5px;
+//   cursor: pointer;
+//   background-color: ${(props) => props.color};
+// `;
+// const FilterSize = styled.select`
+//   margin-left: 10px;
+//   padding: 5px;
+// `;
 
-  &:hover {
-    background-color: rgb(132, 165, 157);
-  }
-`;
+// const FilterSizeOption = styled.option``;
+
+// const AddContainer = styled.div`
+//   width: 50%;
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   ${mobile({ flexDirection: "column" })}
+// `;
+// const AmountContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+//   font-weight: 700;
+// `;
+
+// const Amount = styled.span`
+//   width: 30px;
+//   height: 30px;
+//   border-radius: 10px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   margin: 0px 5px;
+//   border: 1px solid rgb(132, 165, 157);
+//   ${mobile({ marginBottom: "20px" })}
+// `;
+
+// const Button = styled.button`
+//   padding: 15px;
+//   border-radius: 5px;
+//   border: 2px solid rgb(132, 165, 157);
+//   background-color: white;
+//   cursor: pointer;
+//   font-weight: 500;
+
+//   &:hover {
+//     background-color: rgb(132, 165, 157);
+//   }
+// `;
 
 function Product() {
   const [data, setData] = useState("");
@@ -121,13 +152,15 @@ function Product() {
     <Contaier>
       <Announce />
       <Wrapper>
-        {data &&
-          data.map((item) => (
-            <div key={item.id}>
-              <Title>{item.title}</Title>
-              <Image src={item.image}></Image>
-            </div>
-          ))}
+        <Goods>
+          {data &&
+            data.map((item) => (
+              <Wrap key={item.id}>
+                <Title>{item.title}</Title>
+                <Image src={item.image}></Image>
+              </Wrap>
+            ))}
+        </Goods>
       </Wrapper>
       <Newsletter />
     </Contaier>
